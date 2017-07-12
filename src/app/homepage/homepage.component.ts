083e012b646ef12a1ac95759238a174c0bceb249
Christian Thöni <christian@thoeni.me>
2017-07-12 15:20:50 +0200

    Display last import date on frontpage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientsService } from '../clients.service';
 import { SearchService } from '../search.service';
+import { SettingsService, Settings } from "../settings.service";
 
 @Component({
   selector: 'homepage',
@@ -10,12 +11,15 @@ import { SearchService } from '../search.service';
 export class HomepageComponent implements OnInit {
 
   resultsList:Array<any>;
+  settings: Settings;
 
   ngOnInit() {
-  }
+    this.settings = this.settingsService.settings;
+    }
+
 
   searchResults:any;
-  constructor( private searchService:SearchService) {
+  constructor( private searchService:SearchService, private settingsService:SettingsService) {
     this.searchService.searchResult$.subscribe((results) => {
       console.log("recieved update from searchService.searchResult");  
       this.resultsList = this.searchService.resultsList;
