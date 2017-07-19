9e7e73c276150f8edc90e57bc6e985d381dbcf08
Christian Thöni <christian@thoeni.me>
2017-07-19 12:18:12 +0200

    advanced php interface, added security warning

--- src/php-interface.php
+++ src/php-interface.php
@@ -1,19 +1,46 @@
 <?php
 
+/*************************
+CAUTION / WARNING / DANGER
+
+This file is not protected against any type of Injections, in an non-development, productive environment it can produce real harm.
+Ensure to secure the inputs as well as provide any authentification between the main app and this pseudo-interface
+*************************/
+
 switch($_POST['type']) {
     case "file":
 
-        $info = pathinfo($_FILES['userFile']['name']);
-        $ext = $info['extension']; // get the extension of the file
-        $newname = "newname.".$ext; 
+        
+        $info = pathinfo($_FILES['uploadFile']['name']);
+        $imageFileType = $info['extension']; // get the extension of the file
+        
+        $target = 'assets/icons/stores/'.$_POST["filename"].'_'.$_POST['size'].'.'.$imageFileType;
+        
+        // Allow certain file formats
+        if($imageFileType != "png" && $imageFileType != "svg") {
+            echo '{"error": "Nur .PNG und .SVG erlaubt"}';
+            $uploadOk = 0;
+        } else {
 
-        $target = 'images/'.$newname;
-        move_uploaded_file( $_FILES['userFile']['tmp_name'], $target);
+            // Check if file already exists, if yes delete the old one
+            if (file_exists($target_file)) {
+                unlink($file);
+            }   
 
-        echo "200";
+            if (move_uploaded_file($_FILES["uploadFile"]["tmp_name"], $target)) {
+                // echo "The file ". basename( $_FILES["uploadFile"]["name"]). " has been uploaded.";
+                echo '{"fileurl": "'.$target.'"}';
+            } else {
+                echo '{"error": "Hochladen des Bilder fehlgeschlagen"}';
+            }
+        }
+    break;
+    case "storage":
 
     break;
-
+    default:
+    	echo '{"error": "Type nicht erkannt"}';
+    break;
 }
 
 
